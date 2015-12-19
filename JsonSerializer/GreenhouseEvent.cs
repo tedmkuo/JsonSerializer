51ed9b9f1198ab52b8d994a0aef2226c012ac770
tedmkuo <tedmkuo@gmail.com>
2015-12-18 16:25:29 -0800

    Testing commit to GitHub

---
+++ JsonSerializer/GreenhouseEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonSerializer
+{
+    class GreenhouseEvent
+    {
+        public string Action { get; set; }
+        public Payload Payload { get; set; }
+    }
+
+    class Payload
+    {
+        public int Web_hook_id { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+        public string Name { get; set; }
+        public string Event { get; set; }
+        public bool Active { get; set; }
+    }
+}
---
+++ JsonSerializer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JsonSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonDataFile = "JsonInput.txt";
+            var jsonString = File.ReadAllText(jsonDataFile);
+            var greenhouseEvent = JsonConvert.DeserializeObject<GreenhouseEvent>(jsonString);
+            Console.WriteLine($"Action: {greenhouseEvent.Action}");
+            Console.WriteLine($"Event: {greenhouseEvent.Payload.Event}");
+            Console.ReadKey();
+        }
+    }
+}
